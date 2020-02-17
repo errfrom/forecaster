6dc37c47836ea331ace2b1143c674b89283981d5
Dmitri Schuiski <errfrom@yandex.ru>
2020-02-17 22:03:06 +0300

    Initial Commit

---
+++ Forecaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forecaster
+//
+//  Created by Dmitri Schuiski on 2/17/20.
+//  Copyright © 2020 Dmitri Schuiski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
